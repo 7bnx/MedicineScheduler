e979258dee44731278c4706da8bf4f9d7af85027
Semyon Ivanov <agreement90@mail.ru>
2023-08-01 22:25:16 +0300

    feat(entity): add configuration, migration (#1)

--- DataAccessLevel/Migrations/20230801184933_AddEntityConfigurations.Designer.cs
+++ DataAccessLevel/Migrations/20230801184933_AddEntityConfigurations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MedicineScheduler.DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MedicineScheduler.DataAccessLayer.Migrations
 {
     [DbContext(typeof(EFContext))]
-    partial class EFContextModelSnapshot : ModelSnapshot
+    [Migration("20230801184933_AddEntityConfigurations")]
+    partial class AddEntityConfigurations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.9");
